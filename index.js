65b2ddb5afece09b7ba31677be73c8e0aa478b5b
Raúl del Río Catalán <raul@devgurus.io>
2021-01-18 12:23:00 +0100

    Commiteada máxima para hacer pruebas cloud

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
+const NodeGit = require('nodegit');
 const { 
   deleteFolderRecursive,
   cloneRepo,
   fetchRepo,
   createBranchFromMaster,
   upsertFileAndCommit,
+  pushBranchToRemote,
+  createPullRequest,
+  listPullRequest,
 } = require('./services/nodegit');
+
+const GITHUB_TOKEN = '5577caf3ed77a06a4be9ec15fb93ea249c622db4';
 
 const testinggitoperations = async () => {
   const cloneURL = 'https://github.com/rauldel-DG/test-for-nodegit.git';
@@ -13,21 +19,34 @@ const testinggitoperations = async () => {
   
   // Cloning Repo
   let repository = await cloneRepo(cloneURL, localPath);
+  listPullRequest();
 
   // Check current branch (should be master)
   const head = await repository.getCurrentBranch();
   console.log(JSON.stringify(head.name()));
 
   // Create new branch
-  const newBranch = await createBranchFromMaster(repository, 'testarino');
+  const newBranch = await createBranchFromMaster(repository, 'project-config/testarinoRun');
   console.log('BRANCH CREATED -> ', newBranch.name());
 
   // Move to new branch
   await repository.checkoutBranch(newBranch, {});
 
+  const cId = await upsertFileAndCommit(
+    repository,
+    'data',
+    'demo.txt',
+    'Is this the real life? Is this just fantasy?');
+
+  console.log('COMMIT ID -> ', cId);
+
+  await pushBranchToRemote(repository, newBranch, GITHUB_TOKEN);
+
   // Check current branch (should be new one)
   const head2 = await repository.getCurrentBranch();
   console.log(JSON.stringify(head2.name()));
+
+  await createPullRequest();
 
   // Remove temporary cloned repo
   deleteFolderRecursive(localPath);
--- services/nodegit/index.js
+++ services/nodegit/index.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
+const path = require('path');
 const NodeGit = require('nodegit');
+const { Octokit } = require("@octokit/rest");
 
 const deleteFolderRecursive = (path) => {
   if(fs.existsSync(path)) {
@@ -49,9 +51,89 @@ const createBranchFromMaster = async (repository, newBranchName) => {
   return res;
 };
 
+/**
+ * Upserts a file, adds it to current branch and commits it.
+ * @param {Repository} repository Repository instance
+ * @param {string} filePath File path that locates desired file
+ * @param {string} fileName Name of the file to write
+ * @param {Object} fileContent Content of the file
+ * 
+ * @return The Commit Id we just created
+ */
 const upsertFileAndCommit = async (repository, filePath, fileName, fileContent) => {
+  let index;
+  let oid;
+  const pathos = path.join(repository.workdir(), filePath, fileName);
 
+  fs.writeFileSync(pathos, fileContent);
+
+  index = await repository.refreshIndex();
+  index.addByPath(path.posix.join(filePath, fileName));
+  index.write();
+  oid = await index.writeTree();
+
+  const head = await NodeGit.Reference.nameToId(repository, "HEAD");
+  const parentCommit = await repository.getCommit(head);
+  const author = NodeGit.Signature.now("R del Rio", "raul@devgurus.io");
+  const committer = NodeGit.Signature.now("R del Rio's Code", "raul@devgurus.io");
+  const commitMessage = "This is a test message, please take care.";
+
+  const commitId = await repository.createCommit("HEAD", author, committer, commitMessage, oid, [parentCommit]);
+
+  return commitId;
 };
+
+const pushBranchToRemote = async (repository, branch, GITHUB_TOKEN) => {
+  const remote = await repository.getRemote('origin');
+
+  try {
+    await remote.push(
+      [`${branch.name()}:${branch.name()}`],
+      {
+        callbacks: {
+          credentials: function() {
+            return NodeGit.Cred.userpassPlaintextNew(GITHUB_TOKEN, "x-oauth-basic");
+          }
+      }
+      });
+  } catch (err) {
+    console.log('error -> ', err);
+  }
+};
+
+const createPullRequest = async () => {
+  const GITHUB_TOKEN = '5577caf3ed77a06a4be9ec15fb93ea249c622db4';
+  const octokit = new Octokit({
+      auth: GITHUB_TOKEN,
+  });
+
+  const res = await octokit.pulls.create({
+    owner: 'rauldel-DG',
+    repo: 'test-for-nodegit',
+    title: 'Demo PR. All time all around',
+    head: 'testarino',
+    base: 'master',
+  });
+
+  console.log('This is a PR -> ', res);
+}
+
+const listPullRequest = async () => {
+  const GITHUB_TOKEN = '5577caf3ed77a06a4be9ec15fb93ea249c622db4';
+  const octokit = new Octokit({
+      auth: GITHUB_TOKEN,
+  });
+
+  const res = await octokit.pulls.list({
+    owner: 'rauldel-DG',
+    repo: 'test-for-nodegit',
+    state: 'open',
+    /* head: 'testarino',
+    base: 'master', */
+  });
+
+  console.log('RES -> ', res.data);
+}
 
 module.exports = {
   deleteFolderRecursive,
@@ -59,4 +141,7 @@ module.exports = {
   fetchRepo,
   createBranchFromMaster,
   upsertFileAndCommit,
+  pushBranchToRemote,
+  createPullRequest,
+  listPullRequest,
 };
